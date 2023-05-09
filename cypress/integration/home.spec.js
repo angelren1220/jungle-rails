609a9be1c131a05de9c309ddb56f6d654e242943
Linuxe <angelren1220@gmail.com>
2023-05-09 17:10:40 -0400

    add home-page tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,9 @@
+it("There is products on the page", () => {
+  cy.visit("/");
+  cy.get(".products article").should("be.visible");
+});
+
+it("There are 2 products on the page", () => {
+  cy.visit("/");
+  cy.get(".products article").should("have.length", 2);
+});
