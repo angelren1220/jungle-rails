c23fe7c20bc3537704b5114113413ca77ae5fe4a
Linuxe <angelren1220@gmail.com>
2023-05-09 19:44:25 -0400

    add test for product page add to cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,13 +1,7 @@
-describe("Product", () => {
-
-  beforeEach(() => {
-    cy.visit('/') 
-    cy.get(".products article")
-      .should("be.visible")
-      .should("have.length", 2);
-  })
+describe("Cart", () => {
 
   it("At home page: product is added to Cart and the cart should be increased by 1 ", () => {
+    cy.visit('/');
     // My cart originally has 0 item
     cy.get('nav')
       .contains('My Cart (0)');
@@ -15,11 +9,43 @@ describe("Product", () => {
     // Add one item
     cy.get('button')
       .contains('Add')
-      .click({force: true})
-    
+      .click({ force: true });
+
     // My cart has been added 1 item
     cy.get('nav')
       .contains('My Cart (1)');
+  });
+
+  it("At product page: product added to Cart and the cart should be increased by 1 ", () => {
+    // click to visit product page
+    cy.visit("/products/1");
+
+    // My cart originally has 0 item
+    cy.get('nav')
+      .contains('My Cart (0)');
+
+    // Add one item
+    cy.get('button')
+      .contains('Add')
+      .click({ force: true });
+
+    // My cart has been added 1 item
+    cy.get('nav')
+      .contains('My Cart (1)');
+
+    cy.visit("/products/2");
+    // My cart originally has 1 item
+    cy.get('nav')
+      .contains('My Cart (1)');
+
+    // Add one item
+    cy.get('button')
+      .contains('Add')
+      .click({ force: true });
+
+    // My cart has been added 2 items
+    cy.get('nav')
+      .contains('My Cart (2)');
   });
 
 });
