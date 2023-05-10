598470f1042d64b4c5c2fdb604a8bec22730a814
Linuxe <angelren1220@gmail.com>
2023-05-10 13:31:08 -0400

    refactor cypress tests for home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,13 @@
-it("There is products on the page", () => {
-  cy.visit("/");
-  cy.get(".products article").should("be.visible");
-});
+describe("Home Page", () => {
 
-it("There are 2 products on the page", () => {
-  cy.visit("/");
-  cy.get(".products article").should("have.length", 2);
+  it("There are products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
+  
+  it("There are 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+  });
+
 });
